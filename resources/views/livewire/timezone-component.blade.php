30c9175159616382c268e4d8669b5ad789465f9d
Volodymyr Honcharov <ladimirvm87@gmail.com>
2024-04-24 17:35:03 +0300

    Replace form button to select element

--- resources/views/livewire/timezone-component.blade.php
+++ resources/views/livewire/timezone-component.blade.php
@@ -1,19 +1,17 @@
 <div>
     <h1>Timezone App</h1>
-    {{-- @if($showForm) --}}
-        <form wire:submit.prevent="getTime">
-            <label for="timezone">Select Timezone:</label>
-            <select wire:model="selectedTimezone" id="timezone">
-                @foreach($timezones as $tz)
-                    <option value="{{ $tz }}">{{ $tz }}</option>
-                @endforeach
-            </select>
-            <button type="submit">Get Time</button>
-        </form>
-    {{-- @endif --}}
+    <label for="timezone">Select Timezone:</label>
+    <select wire:model="selectedTimezone" id="timezone" wire:change="getTime">
+        @foreach($timezones as $tz)
+            <option value="{{ $tz }}">{{ $tz }}</option>
+        @endforeach
+    </select>
+
     @if($currentTime)
         <p>Time in Selected Timezone: {{ $currentTime }}</p>
     @else
         <p>Current Time: {{ Carbon\Carbon::now() }}</p>
     @endif
 </div>
+
+
